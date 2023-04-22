7fbc5969be3229977128712f6d80ab378e65a656
souravadhikar1 <souravadhikari09296@gmail.com>
2023-04-22 14:37:14 +0600

    habijabi layout making done

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Category = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>This is Category{id}</h2>
+    </div>
+  );
+};
+
+export default Category;
