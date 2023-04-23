eb4e6eb0ad8a8416fe178bbd59fed8cbe799d0bf
souravadhikar1 <souravadhikari09296@gmail.com>
2023-04-23 08:46:04 +0600

    editors insights section added

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { FaArrowLeft } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
+import EditorsInsights from "../Editorsinside/EditorsInsights";
 
 const News = () => {
   const news = useLoaderData();
@@ -9,19 +10,25 @@ const News = () => {
   const { _id, title, details, image_url, category_id } = news;
 
   return (
-    <Card>
-      <Card.Img variant="top" src={image_url} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{details}</Card.Text>
-        <Link to={`/category/${category_id}`}>
-          <Button variant="danger" className="fw-semibold">
-            {" "}
-            <FaArrowLeft></FaArrowLeft> All News in this Category
-          </Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger" className="fw-semibold">
+              {" "}
+              <FaArrowLeft></FaArrowLeft> All News in this Category
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="mt-3">
+        <h4 className="fw-bold text-black">Editors Insight</h4>
+        <EditorsInsights></EditorsInsights>
+      </div>
+    </div>
   );
 };
 
