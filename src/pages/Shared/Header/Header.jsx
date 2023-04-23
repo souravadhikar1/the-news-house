4a7e8bca49275e22ac2ebc295ff032de8f6b734a
souravadhikar1 <souravadhikari09296@gmail.com>
2023-04-23 09:37:05 +0600

    navigationBar done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment/moment";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../../provider/AuthProvider";
-import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
-  const { user } = useContext(AuthContext);
-
   return (
     <Container className="mt-4">
       <div className="text-center">
@@ -27,37 +21,6 @@ const Header = () => {
           text.
         </Marquee>
       </div>
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link href="#features">
-                <Link to="/">Home</Link>
-              </Nav.Link>
-              <Nav.Link href="#pricing">About</Nav.Link>
-              <Nav.Link href="#pricing">Career</Nav.Link>
-            </Nav>
-            <Nav>
-              {user && (
-                <Nav.Link href="#deets">
-                  <FaUserCircle style={{ fontSize: "2rem" }}></FaUserCircle>
-                </Nav.Link>
-              )}
-              <Nav.Link eventKey={2} href="#memes">
-                {user ? (
-                  <Button variant="secondary">Logout</Button>
-                ) : (
-                  <Link to="/login">
-                    <Button variant="secondary">Login</Button>:
-                  </Link>
-                )}
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
     </Container>
   );
 };
