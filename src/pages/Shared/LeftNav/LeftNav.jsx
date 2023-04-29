7cf48efdda10cf425f04150e77ca30b26b264185
souravadhikar1 <souravadhikari09296@gmail.com>
2023-04-29 08:04:05 +0600

    final touch added and react awsesome componets added

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -5,7 +5,9 @@ const LeftNav = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/categories")
+    fetch(
+      "https://the-news-daragon-server-souravadhikar1.vercel.app/categories"
+    )
       .then((res) => res.json())
       .then((data) => setCategories(data))
       .catch((error) => console.error(error));
