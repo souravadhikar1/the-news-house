5d1be6e18c1895f10660756a930202e5e4268c1c
souravadhikar1 <souravadhikari09296@gmail.com>
2023-04-29 06:59:50 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log("environment variable", import.meta.env.VITE_apiKey);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCH8j5wMRXquNSiQFaZEBT_ZBRxltsidBs",
-  authDomain: "news-daragon.firebaseapp.com",
-  projectId: "news-daragon",
-  storageBucket: "news-daragon.appspot.com",
-  messagingSenderId: "666921927828",
-  appId: "1:666921927828:web:cc4fbcce02aeb65cb76081"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -36,7 +36,7 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
-      console.log("logged in user", loggedUser);
+      // console.log("logged in user", loggedUser);
       setUser(loggedUser);
       setLoading(false);
     });
